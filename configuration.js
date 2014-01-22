ceba7dc62fe5e350bdff71664755e2b91b77a92b
Erika Suárez Valencia <marvinesv@gmail.com>
2014-01-22 17:31:27 -0500

    Mas pruebas rancias para comparar

--- configuration.js
+++ configuration.js
@@ -2,6 +2,6 @@ var config2 = {
   urlServer: 'http://localhost:8000'
 }
 var config1 = {
-  urlServer: 'http://eva05.local:8000'
+  urlServer: 'http://eva04.local:8000'
 }
-module.exports = config2;
+module.exports = config1;
