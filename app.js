16b102014821c5d6d4d1064f6c857491576062fe
Andrés Ricardo Torres Martínez <torresandresricardo@gmail.com>
2015-02-25 19:09:15 -0500

    Coffee

--- app.js
+++
@@ -1,5 +0,0 @@
-var Client = require('./lib/client.js');
-
-var aClient = new Client();
-
-aClient.requestJob();
--- config/configuration.js
+++
@@ -1,7 +0,0 @@
-var config2 = {
-  urlServer: 'http://localhost:8000'
-}
-var config1 = {
-  urlServer: 'http://eva04.local:8000'
-}
-module.exports = config2;
